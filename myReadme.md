fb93be281795f6496d40824fa68919de5267f8bc
Shimul-Github <shimulganankaj@gmail.com>
2025-08-15 01:31:20 +0200

    'rearranged'

--- myReadme.md
+++ myReadme.md
@@ -1,5 +1,5 @@
 Awsome React Components : https://github.com/brillout/awesome-react-components
 For Banner: I use react awsome components - 
-Shortcuts: alt + blackslash , alt + shift + down arrow > for copy and paste , 
+Shortcuts: alt + blackslash , alt + shift + down arrow > for copy and paste , alt + shift + F> To align codes
 
 Index.css file daisyui er plugin er niche lal dag acche
--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,45 +1,30 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import img1 from "../../assets/Banner/1.jpg";
+import img2 from "../../assets/Banner/2.jpg";
+import img3 from "../../assets/Banner/3.jpg";
+import img4 from "../../assets/Banner/4.jpg";
+import img5 from "../../assets/Banner/5.jpg";
+import img6 from "../../assets/Banner/6.jpg";
 
-import img1 from '../../assets/Banner/1.jpg'
-import img2 from '../../assets/Banner/2.jpg'
-import img3 from '../../assets/Banner/3.jpg'
-import img4 from '../../assets/Banner/4.jpg'
-import img5 from '../../assets/Banner/5.jpg'
-import img6 from '../../assets/Banner/6.jpg'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Banner = () => {
   return (
     <>
-      <Swiper
-        className="mySwiper swiper-h"
-        direction={'horizontal'}
-        spaceBetween={50}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-      >
-        <SwiperSlide> Horizontal
-          <img className="w-50%" src={img1} alt="" />
-        </SwiperSlide>
-        
-        <SwiperSlide> Vertical
-          <img  className="w-50%" src={img4} alt="" />
-
-          
-        </SwiperSlide>
-       
-        <SwiperSlide>
-          <img  className="w-50%" src={img3} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img  className="w-50%" src={img4} alt="" />
-        </SwiperSlide>
-       
-      </Swiper>
+      <Carousel>
+        <div>
+        <img src={img1} />
+          <p className="legend text-white">Legend 1</p>
+        </div>
+        <div>
+          <img src={img1} />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src={img1} />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
     </>
   );
 };
